b346893aa7efe7fee06a692073eb936d76cab585
allysontrinh <allysontrinh@gmail.com>
2023-11-11 13:23:23 -0500

    Initial Commit

---
+++ bubbleUp/bubbleUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  bubbleUpApp.swift
+//  bubbleUp
+//
+//  Created by Allyson Trinh on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct bubbleUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
